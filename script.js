122bd1abc8c8799673050f494027334fde9c9d05
PsalmzyFX <samsonchinemerem0@gmail.com>
2024-11-06 20:46:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+// Function for Sign Up button
+function signUp() {
+    alert("Sign Up function clicked! Redirecting to sign up page...");
+    // Redirect to Sign-Up Page (not implemented here)
+}
+
+// Function for Sign In button
+function signIn() {
+    alert("Sign In function clicked! Redirecting to sign in page...");
+    // Redirect to Sign-In Page (not implemented here)
+}
+
+// Function for the Alert Button
+function showAlert() {
+    alert("Welcome to Tradelynx Platform! Discover endless investment opportunities.");
+}
